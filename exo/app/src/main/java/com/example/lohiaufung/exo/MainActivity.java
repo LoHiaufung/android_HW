ee9b7b6464cb6890b7b7832a68130398405e211e
LoHiaufung <243540158@qq.com>
2017-12-27 22:00:29 +0800

    JiaoziDemo

---
+++ exo/app/src/main/java/com/example/lohiaufung/exo/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.lohiaufung.exo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.jzvd.JZVideoPlayerStandard;
+import cn.jzvd.JZVideoPlayer;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        JZVideoPlayerStandard jzVideoPlayerStandard = (JZVideoPlayerStandard) findViewById(R.id.videoplayer);
+        jzVideoPlayerStandard.setUp("http://jzvd.nathen.cn/c6e3dc12a1154626b3476d9bf3bd7266/6b56c5f0dc31428083757a45764763b0-5287d2089db37e62345123a1be272f8b.mp4"
+                , JZVideoPlayerStandard.SCREEN_WINDOW_NORMAL, "饺子闭眼睛");
+        jzVideoPlayerStandard.thumbImageView.setImage("http://p.qpic.cn/videoyun/0/2449_43b6f696980311e59ed467f22794e792_1/640");
+    }
+    @Override
+    public void onBackPressed() {
+        if (JZVideoPlayer.backPress()) {
+            return;
+        }
+        super.onBackPressed();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        JZVideoPlayer.releaseAllVideos();
+    }
+}
